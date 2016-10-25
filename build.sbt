7da4a5403ed3af97fbd00aa7bccda0b5e05fdac4
Antoine Comte <antoine@comte.cc>
2016-10-25 22:11:07 +0200

    dom

--- build.sbt
+++ build.sbt
@@ -2,3 +2,5 @@ name := "Scala.Io demo"
 scalaVersion := "2.11.8"
 
 enablePlugins(ScalaJSPlugin)
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
--- src/main/scala/io.scala.scalajs/App.scala
+++ src/main/scala/io.scala.scalajs/App.scala
@@ -2,8 +2,14 @@ package io.scala.scalajs
 
 import scala.scalajs.js.JSApp
 
+import org.scalajs.dom
+import dom.document
+
 object DemoApp extends JSApp {
   def main(): Unit = {
-    println("Hello Scala.io!")
+    val hello= document.createTextNode("Hello Scala.io")
+    val para = document.createElement("p")
+    para.appendChild(hello)
+    document.body.appendChild(para)
   }
 }
